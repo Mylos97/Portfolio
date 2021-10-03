a37d8ad0af4a218d0996bc9c39dab0cd2ccb25c1
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-10-03 17:43:10 +0200

    swag

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {Header, Footer, ShowingPage} from './components' 
-
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./client";
 
 function App() {
   return (
+    <ApolloProvider client= {client}>
     <div className="App">
-    <Header/>
-    <ShowingPage/>
-    <Footer/>
+      <Header/>
+      <ShowingPage/>
+      <Footer/>
     </div>
+    </ApolloProvider>
   );
 }
 
--- src/components/oldprojects/index.js
+++ src/components/oldprojects/index.js
@@ -1,12 +1,36 @@
 import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+
+const ITEMS_QUERY = gql`
+  {
+    allBlogPosts {
+      data {
+        _id
+        title
+        date
+        content
+      }
+    }
+  }
+`;
 
 const OldProjects = () => {
+  const { data, loading } = useQuery(ITEMS_QUERY);
 
-    return (
-        <div>
-            OOOLD PROJECTS
-        </div>
-    )
-}
+  return (
+    <div>
+      OOOLD PROJECTS
+      {loading && <div>loading</div>}
+      {data && (
+        <ul>
+          {data.allBlogPosts.data.map((blogpost) => {
+            return <li key={blogpost._id}>{blogpost.title}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default OldProjects;
