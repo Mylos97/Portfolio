8243d2d22e751c41daa04ac483504dddfce80af4
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-10-04 16:05:02 +0200

    please build

--- src/components/blogpost/index.js
+++ src/components/blogpost/index.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { useState } from "react";
 import "./index.css";
  
 const BlogPost = ({ title, date, content, images }) => {
-  const [blogTitle, setBlogTitle] = useState(title);
-  const [blogDate, setBlogDate] = useState(date);
-  const [blogContent, setBlogContent] = useState(content);
+  //const [blogTitle, setBlogTitle] = useState(title);
+  //const [blogDate, setBlogDate] = useState(date);
+  //const [blogContent, setBlogContent] = useState(content);
 
   return (
     <div className='blog-post'>
-        <h2>{blogTitle}</h2>
-        <h3>{blogDate}</h3>
-        <p>{blogContent}</p>
+        <h2>{title}</h2>
+        <h3>{date}</h3>
+        <p>{content}</p>
     </div>
   );
 };
