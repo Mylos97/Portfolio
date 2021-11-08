fe2c1e3d362cca1cc9bf88139614b4cf63a6866d
Mylos97 <57527830+Mylos97@users.noreply.github.com>
2021-11-08 21:27:39 +0100

    a lot of work

--- src/components/blogpost/index.js
+++ src/components/blogpost/index.js
@@ -5,30 +5,97 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/client";
 import { validateBlogPost } from "../../utils/validateblogpost";
 
-
 const UPDATE_BLOG_POST = gql`
-  mutation UpdateBlogPost($id: ID!, $data:BlogPostInput!) {
+  mutation UpdateBlogPost($id: ID!, $data: BlogPostInput!) {
     updateBlogPost(id: $id, data: $data) {
       _id
       title
       date
       content
+      images
     }
   }
 `;
 
-
-const BlogPost = ({ blogId, title, date, content, images, adminLoggedIn }) => {
+const BlogPost = ({ blogId, title, date, content, imgs, adminLoggedIn }) => {
   const [blogTitle, setBlogTitle] = useState(title);
   const [blogDate, setBlogDate] = useState(date);
   const [blogContent, setBlogContent] = useState(content);
+  const [images, setImages] = useState(imgs);
   const [editable, setEditable] = useState(false);
-  const [submitEditBLogPost] = useMutation(UPDATE_BLOG_POST)
+  const [files, setFiles] = useState();
+  const [submitEditBLogPost] = useMutation(UPDATE_BLOG_POST);
 
   const updateBlogPost = () => {
     if (!validateBlogPost(blogTitle, blogDate, blogContent)) return;
-    setEditable(false)
-    submitEditBLogPost({variables:{id:blogId, data:{title:blogTitle, date:blogDate, content:blogContent}}})
+    setEditable(false);
+    let deleteImg = deleteImages();
+    submitEditBLogPost({
+      variables: {
+        id: blogId,
+        data: {
+          title: blogTitle,
+          date: blogDate,
+          content: blogContent,
+          images: images,
+        },
+      },
+    });
+  };
+
+  async function deleteImages() {
+    const url = "http://localhost:5000/delete";
+    let promise = new Promise((resolve, reject) => {
+      for (let i = 0; i < images.length; i++) {
+        let tmpImage = images[i];
+        const formData = new FormData();
+        formData.append("image", tmpImage);
+        fetch(url, {
+          method: "POST",
+          body: formData,
+        })
+          .then((res) => {
+            console.log(res);
+          })
+          .then(() => {
+            if (i === images.length - 1) resolve("Removed the correct images");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+    return promise;
+  }
+
+  async function changeUploaded(e) {
+    e.preventDefault();
+    let imagesNames = [];
+    const url = "https://secret-spire-88724.herokuapp.com/upload";
+    if (!files) return;
+    let promise = new Promise((resolve, reject) => {
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        const formData = new FormData();
+        formData.append("image", file);
+        fetch(url, {
+          method: "POST",
+          body: formData,
+        })
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            imagesNames.push(data.url);
+            if (i === files.length - 1) resolve("done");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+    promise.then(() => window.alert("Finished uploading"));
+    setImages(imagesNames);
   }
 
   return (
@@ -57,9 +124,31 @@ const BlogPost = ({ blogId, title, date, content, images, adminLoggedIn }) => {
       >
         {blogContent}
       </p>
+      {images && (
+        <ul className="images">
+          {images.map((url, index) => {
+            return (
+              <li key={index}>
+                <img src={url} alt="Cannot load"></img>
+              </li>
+            );
+          })}
+        </ul>
+      )}
       {adminLoggedIn && (
         <div className="edit-btns">
           <button onClick={() => setEditable(!editable)}>Edit Me!</button>
+          {editable && (
+            <>
+              <input
+                type="file"
+                onChange={(e) => setFiles(e.target.files)}
+                accept=".jpg, .png, .jpeg"
+                multiple={true}
+              ></input>
+              <button onClick={(e) => changeUploaded(e)}>Upload Images</button>
+            </>
+          )}
           <button onClick={() => updateBlogPost()}>Submit changes</button>
         </div>
       )}
